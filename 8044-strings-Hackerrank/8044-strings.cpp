ed6aab6907467999f9fb61c536ad333d3698404c
iswarya0811 <iswaryalakshmi0811@gmail.com>
2026-02-23 21:12:49 +0530

    Added solution - LeetHub

---
+++ 8044-strings-Hackerrank/8044-strings.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	// Complete the program
+    string a,b;
+    cin >> a >> b;
+    cout << a.size() << " " << b.size() << endl;
+    cout << a+b << endl;
+    cout << b.substr(0,1)+a.substr(1,a.size()-1) << " " << a.substr(0,1) +b.substr(1,b.size()-1);
+  
+    return 0;
+}
