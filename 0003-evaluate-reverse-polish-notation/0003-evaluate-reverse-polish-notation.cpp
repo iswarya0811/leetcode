b6dec64c6f667508f8657b595179212e05de1b8b
iswarya0811 <iswaryalakshmi0811@gmail.com>
2026-07-13 15:39:33 +0530

    Time: 3 ms (35.92%), Space: 17.2 MB (41.09%) - LeetHub - Time: O(, Space: O(?)

---
+++ 0003-evaluate-reverse-polish-notation/0003-evaluate-reverse-polish-notation.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int>st;
+        int res;
+       for(auto i:tokens)
+       {
+        if(i == "+" || i == "-" || i=="*" || i == "/")
+        {
+            int b=st.top();
+            st.pop();
+            int a =st.top();
+            st.pop();
+            char op = i[0];
+            switch(op){
+                case '+':
+                    res=a+b;
+                    break;
+                case '-':
+                    res=a-b;
+                    break;
+                case '*':
+                    res = a*b;
+                    break;
+                case '/':
+                    res = a/b;
+                    break;
+            }
+            st.push(res);
+        }
+        else
+        {
+            st.push(stoi(i));
+        }
+       } 
+       int ans = st.top();
+       return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
