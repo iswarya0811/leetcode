54bfd7a42753206c974d21ad5160f07df639226e
iswarya0811 <iswaryalakshmi0811@gmail.com>
2026-08-24 15:44:55 +0530

    Time: 0 ms (100%), Space: 20 MB (43.99%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,22 @@
+def BinarySearch(nums,target):
+    low = 0
+    high = len(nums)-1
+    while low <= high:
+        mid = (low+high)//2
+        if nums[mid]==target:
+            return mid
+        elif nums[mid]<target:
+            low = mid + 1
+        else:
+            high = mid -1
+    return low
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        return BinarySearch(nums,target)
+
+
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
